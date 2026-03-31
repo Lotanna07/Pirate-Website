8edc6c476c72955cd575bfa3a1b40d19784da0e1
lotanna <lotanna006@gmail.com>
2026-03-31 02:29:30 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,4 +30,10 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () =>{
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                      : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
